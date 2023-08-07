ef42a2e229f58cf925c1c6080339827183e5c45b
Arthur Oliveira <81394800+melloArthur@users.noreply.github.com>
2023-08-07 20:05:03 -0300

    Update main.py
    funções de curva e estacionador.

--- main.py
+++ main.py
@@ -11,10 +11,53 @@ direito = Motor(Port.B, Direction.COUNTERCLOCKWISE)
 garraA = Motor(Port.C, Direction.COUNTERCLOCKWISE)
 garraB = Motor(Port.D)
 
-movimento = DriveBase(esquerdo, direito, 87, 126)
-garra = DriveBase(garraA,garraB,100,100)
+robo = DriveBase(esquerdo, direito, 87, 126)
+garra = DriveBase(garraA,garraB,19,100)
 
 corD = ColorSensor(Port.E)
 corE = ColorSensor(Port.F)
 
+
+hub.system.set_stop_button((Button.BLUETOOTH))
+
 #Ínicio do código
+
+def EsqCurva(ang):
+    if hub.imu.ready() == True:
+        print('oi')
+        while hub.imu.heading() > -(ang):
+            esquerdo.run(100)
+            direito.run(-100)
+        esquerdo.brake()
+        direito.brake()
+        hub.imu.reset_heading(0)
+
+def DirCurva(ang):
+    if hub.imu.ready() == True:
+        print('oi')
+        while hub.imu.heading() < (ang):
+            esquerdo.run(-100)
+            direito.run(100)
+        esquerdo.brake()
+        direito.brake()
+        hub.imu.reset_heading(0)
+
+def Estaciona(cor):
+    while corD.color() != cor and corE.color() != cor:
+        robo.drive(50,0)
+        print (corD.color())
+    robo.stop()
+    if corD.color() == cor:
+        while corE.color() != cor:
+            direito.run(90)
+        wait(150)
+        direito.brake()
+    elif corE.color() == cor:
+        while corD.color() != cor:
+            esquerdo.run(90)
+        wait(150)
+        esquerdo.brake()
+
+Estaciona(Color.NONE)
+robo.straight(-50)
+Estaciona(Color.NONE)
